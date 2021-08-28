0110f404ba32e9e7851cd3c1a90607090d5b6d80
Utkarsh Sharma <54320208+utkarsh24122@users.noreply.github.com>
2021-08-28 09:23:58 +0530

    Update s3extract.sh

--- s3extract.sh
+++ s3extract.sh
@@ -24,11 +24,29 @@ rm js1.txt js2.txt js3.txt
 echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m JS files' links saved in /"$target"_s3/JSfiles.txt \e[0m\n"
 
 
+echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Extracting S3 bucket links \e[0m\n"
+
+cat JSfiles.txt | xargs -I% bash -c ‘curl -sk “%” | grep -w “*.s3.amazonaws.com”’ >> bucket_links.txt
+cat JSfiles.txt | xargs -I% bash -c ‘curl -sk “%” | grep -w “*.s3.us-east-2.amazonaws.com”’ >> bucket_links.txt
+cat JSfiles.txt | xargs -I% bash -c ‘curl -sk “%” | grep -w “s3.amazonaws.com/*”’ >> bucket_links.txt
+cat JSfiles.txt | xargs -I% bash -c ‘curl -sk “%” | grep -w “s3.us-east-2.amazonaws.com/*”’ >> bucket_links.txt
+
+echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m S3 bucket links saved in /"$target"_s3/bucket_links.txt \e[0m\n"
+
+
 echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Extracting S3 bucket names \e[0m\n"
 
-cat JSfiles.txt | xargs -I% bash -c ‘curl -sk “%” | grep -w “*.s3.amazonaws.com”’ >> s3_bucket.txt
-cat JSfiles.txt | xargs -I% bash -c ‘curl -sk “%” | grep -w “*.s3.us-east-2.amazonaws.com”’ >> s3_bucket.txt
-cat JSfiles.txt | xargs -I% bash -c ‘curl -sk “%” | grep -w “s3.amazonaws.com/*”’ >> s3_bucket.txt
-cat JSfiles.txt | xargs -I% bash -c ‘curl -sk “%” | grep -w “s3.us-east-2.amazonaws.com/*”’ >> s3_bucket.txt
+cat bucket_links.txt | sed ‘s/s3.amazonaws.com//’ >> bucket_names.txt
+cat bucket_links.txt | sed ‘s/s3.us-east-2.amazonaws.com//’ >> bucket_names.txt
 
-echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m S3 bucket names saved in /"$target"_s3/s3_bucket.txt \e[0m\n"
+echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m S3 bucket links saved in /"$target"_s3/bucket_names.txt \e[0m\n"
+
+
+echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Checking Bucket Permissions \e[0m\n"
+
+
+
+echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m S3 bucket links saved in /"$target"_s3/bucket_names.txt \e[0m\n"
+
+
+
